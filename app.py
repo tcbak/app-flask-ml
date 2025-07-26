0a44d2a0d6c99a9cead65975b940a1ec663d330d
tcbak <101489127+tcbak@users.noreply.github.com>
2025-07-26 11:29:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def predict():
     prediction = model.predict([np.array(data)])
     return jsonify({'prediction': prediction.tolist()})
 
+@app.route('/')
+def home():
+    return "Welcome to App"
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
